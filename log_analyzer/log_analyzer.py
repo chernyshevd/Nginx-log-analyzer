13cf7d4149495772262f93fc29455a9680312bf6
chernyshevd <dmitrychernyshev@outlook.com>
2024-08-14 22:19:05 +0300

    add project

---
+++ log_analyzer/log_analyzer.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import datetime
+import gzip
+import json
+import logging
+import logging.config
+import os
+import pathlib
+import re
+import statistics
+import string
+import structlog
+import sys
+from typing import Dict, List
+from collections import defaultdict
+
+config = {
+    'TEMPLATE_PATH': './reports/report.html',  # Шаблон исходного отчета
+    'REPORT_SIZE': 100,  # Кол-во записей в итоговом отчете
+    'REPORT_DIR': './reports',  # Путь куда пишется отчет
+    'LOG_DIR': './log',  # Путь откуда читаются исходные логи
+    'ERRORS_LIMIT_PERC': 5,  # Допустимая ошибка парсинга в %
+    'SELF_LOG_PATH': './log/log_analyzer.log',  # Путь к собственныи логам программы
+    'DEBUG_MODE': True,
+    'TEST_SIZE': 1000
+}
+
+
+def parser_args() -> Dict[str, str]:
+    """Парсер аргументов из командной строки"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='./config/config.json',
+                        help='Parameter for passing the config, default=./config/config.json')
+    args = parser.parse_args()
+
+    return args.config
+
+
+def get_result_config(default_config: Dict[str, str], config_path: str) -> Dict[str, str]:
+    """Получим результирующий конфиг"""
+    try:
+        with open(config_path, 'r') as config_file:
+            result_config = json.load(config_file)
+        return {**default_config, **result_config}  # Что бы не трогать дефолтный конфиг!
+    except ValueError:
+        return None
+
+def logger_func(name: str, log_level: int = logging.DEBUG, stdout: bool = True, file: str = None) -> logging.Logger:
+    """
+    Создаем логгер
+    """
+    # Настройка logging для записи в файл
+    if file is not None:
+        logging.basicConfig(filename=file, level=log_level)
+
+    # Настройка structlog для вывода в sys.stdout
+    structlog.configure(
+        processors=[
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer()
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    logger = structlog.get_logger(name)
+
+    # Создание обработчика для вывода в sys.stdout
+    if stdout:
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(log_level)
+
+    # Добавление обработчика к стандартному логгеру logging
+    logging.getLogger().addHandler(stdout_handler)
+    return logger
+
+def search_last_log(log_dir: str, logger: logging.Logger) -> Dict[str, str]:
+    '''
+    Функция крайнего файла логгирования.
+
+    Parameters
+    ----------
+    log_dir : str
+        директория хранения логово.
+    logger : logging.Logger
+        логгер
+
+    Returns
+    -------
+    result : Dict[str, str]
+        Путь до файла лога
+    '''
+
+    logger.info(f'Search logs in directory {log_dir}')
+
+    regex = re.compile(r'nginx-access-ui.log-([\d]{8})(.*)')
+
+    max_dt = datetime.datetime(1, 1, 1)
+    max_path = None
+    max_file = None
+    file_lst = os.listdir(log_dir)
+    i = 0
+    while i < len(file_lst):
+        res = regex.search(file_lst[i])
+        if (res is not None) and (res.group(2) == '.gz' or res.group(2) == ''):
+            dt = datetime.datetime.strptime(res.group(1), '%Y%m%d')
+            path = pathlib.Path(log_dir, file_lst[i])
+            file = res.group(2)
+            if dt > max_dt:
+                max_dt = dt
+                max_path = path
+                max_file = file
+        i += 1
+
+    if max_path is not None:
+        logger.info(f'The logs {max_path} was found')
+        return {'log_path': max_path, 'log_date': max_dt, 'log_file': max_file}
+    else:
+        logger.info('The log file was not found!')
+        return None
+
+
+def get_report_path(report_dir, last_log, logger):
+    '''
+    Проверяем, существует ли отчет с таким именем в указанной dir.
+    Если да, да парсинг выполнялся и прошел успешно возвращаем None, заканчиваем работу.
+    Если нет, возвращаем path для записи отчета
+    '''
+
+    report_name = f"report-{last_log['log_date'].strftime('%Y.%m.%d')}.html"
+    report_path = pathlib.Path(report_dir, report_name)
+    if os.path.exists(report_path):
+        logger.info(f"The {report_name} report already exists")
+        return None
+    else:
+        logger.info(f"The report will be saved to the {report_name} file")
+        return report_path
+
+
+def parsing_line(line: str, logger: logging.Logger) -> Dict[str, str]:
+    regex = re.compile(r'(?:GET|POST|HEAD|PUT|OPTIONS|DELETE).(.*).HTTP/.* (\d{1,6}[.]\d+)')
+    try:
+        res_regex = regex.search(line)
+    except UnicodeError:
+        logger.exception(f'Recording decoding error: {line}')
+        res = {'url': None, 'request_time': None, 'parsing_status': 'error'}
+        return res
+
+    if res_regex is None:
+        logger.debug(f'Record parsing error: {line}')
+        res = {'url': None, 'request_time': None, 'parsing_status': 'not_parsed'}
+        return res
+
+    url = res_regex.group(1).strip()
+    request_time = float(res_regex.group(2).strip())
+    res = {'url': url, 'request_time': request_time, 'parsing_status': 'success'}
+    return res
+
+
+def log_statistic_calc(latest_log: Dict, result_config: Dict, logger: logging.Logger) -> List[Dict]:
+
+    log_path = latest_log['log_path']
+    log_file = latest_log['log_file']
+
+    not_parsed_line = 0
+    time_sum_all_req = 0
+    logs_cnt = 0
+    url_time_list = defaultdict(list)
+
+    try:
+        log_file = gzip.open(log_path, 'rb') if log_file == '.gz' else open(log_path, 'rb')
+    except OSError:
+        logger.error(f'The {log_path} file does not open')
+
+    for line in log_file:
+
+        if result_config['DEBUG_MODE'] and logs_cnt == result_config['TEST_SIZE']:
+            break
+        logs_cnt += 1
+        line = line.decode('utf-8')
+        parsed_line = parsing_line(line, logger)
+        if parsed_line['parsing_status'] == 'error':
+            logger.error('Error parsing logs')
+            return None
+        elif parsed_line['parsing_status'] == 'not_parsed':
+            not_parsed_line += 1
+            continue
+
+        url = parsed_line['url']
+        request_time = parsed_line['request_time']
+        time_sum_all_req += request_time
+
+        url_time_list[url].append(request_time)
+    url_stat = {}
+
+    logger.info(f'{logs_cnt} logs were found in the file {log_path}')
+    logger.info(f'{not_parsed_line} logs were not parsed')
+
+    for url in url_time_list:
+        url_stat_temp = {}
+        url_stat_temp['url'] = url
+        url_stat_temp['count'] = len(url_time_list[url])
+        url_stat_temp['count_perc'] = round(url_stat_temp['count'] / logs_cnt * 100, 3)
+        url_stat_temp['time_avg'] = round(statistics.mean(url_time_list[url]), 3)
+        url_stat_temp['time_max'] = round(max(url_time_list[url]), 3)
+        url_stat_temp['time_med'] = round(statistics.median(url_time_list[url]), 3)
+        url_stat_temp['time_sum'] = round(sum(url_time_list[url]), 3)
+        url_stat_temp['time_perc'] = round(url_stat_temp['time_sum'] / time_sum_all_req * 100, 3)
+        url_stat[url] = url_stat_temp
+
+    res_list = list(url_stat.values())
+    res_list = sorted(res_list, key=lambda x: x['time_sum'], reverse=True)
+    return res_list
+
+
+def html_report_writer(result_config: str, report_path: str, logs_statistic: List[Dict], logger: logging.Logger) -> bool:
+    """
+    Dump the statistics into a json string.
+    Copy the report template to the dir specified in config with the desired name
+    Rendering the report
+    """
+
+    table_json = json.dumps(logs_statistic[:result_config['REPORT_SIZE']])
+    template_path = result_config['TEMPLATE_PATH']
+
+    with open(report_path, 'w', encoding='utf-8') as report, open(template_path, 'r', encoding='utf-8') as tmpl:
+        tmpl_obj = string.Template(tmpl.read())
+        try:
+            render_html = tmpl_obj.safe_substitute(table_json=table_json)
+        except Exception:
+            logger.error('The template could not be rendered!')
+            return False
+        report.write(render_html)
+        logger.info(f'The report {report_path} has been generated!')
+    return True
+
+
+def main():
+    '''
+    1. Получаем результирующий config
+    2. Создаем логера
+    3. Проверяем параметры результирующего config
+    4. Ищем файл последнего лога, если не находим конец
+    5. Проверяем есть ли уже отчет в указанной папке, если находим конец
+    6. Получаем статистику по логам
+    7. Создаем отчет
+    '''
+
+    config_path = parser_args()
+
+
+    result_config = get_result_config(config, config_path)
+
+    # log_path = result_config.get('SELF_LOG_PATH')
+
+    logger = logger_func(__name__, file=result_config['SELF_LOG_PATH'])
+    logger.info(f'Config path {config_path}')
+    logger.info(f'DEBUG_MODE {result_config["DEBUG_MODE"]}')
+
+    latest_log = search_last_log(result_config['LOG_DIR'], logger)
+
+    report_path = get_report_path(result_config["REPORT_DIR"], latest_log, logger)
+
+    if report_path is not None:
+
+        try:
+            logs_statistic = log_statistic_calc(latest_log, result_config, logger)
+        except Exception:
+            logger.error('Emergency termination of the program!!!')
+
+        if logs_statistic is None:
+            logger.debug('Statistics are empty!')
+        else:
+            html_report_writer(result_config, report_path, logs_statistic, logger)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as err:
+        logging.exception(err)
+    except Exception as err:
+        logging.exception(err)
---
+++ tests/test_log_analyzer.py
@@ -0,0 +1,38 @@
+import datetime
+import os
+import sys
+CUR_PATH = os.path.dirname(__file__)
+sys.path.append(os.path.join(CUR_PATH, '..'))
+from log_analyzer import logger_func, search_last_log, parsing_line, get_report_path
+
+logger = logger_func('test')
+
+def test_search_last_log():
+    expected_log_path = 'latest_log/nginx-access-ui.log-20180730'
+    expected_date = datetime.datetime(2018, 7, 30, 0, 0)
+    log_dict = search_last_log('latest_log', logger)
+    log_path = str(log_dict['log_path'])
+    log_date = log_dict['log_date']
+    log_file = log_dict['log_file']
+    assert expected_log_path == log_path
+    assert expected_date == log_date
+    assert '' == log_file
+
+def test_parsing_line():
+
+    test_line = '''1.199.4.96 -  - [29/Jun/2017:03:50:22 +0300]\
+    "GET /api/v2/slot/4705/groups HTTP/1.1" 200 2613"-" \
+    "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" \
+    "-" "1498697422-3800516057-4708-9752745" "2a828197ae235b0b3cb" 0.704\n'''
+
+    expected_data = {'url': '/api/v2/slot/4705/groups', 'request_time': 0.704, 'parsing_status': 'success'}
+    output_data = parsing_line(test_line, logger)
+    assert output_data == expected_data
+
+
+def test_get_report_path():
+    report_dir = 'latest_log'
+    last_log = {'log_date': datetime.datetime(2018, 7, 30, 0, 0)}
+    expected_report_name = 'latest_log/report-2018.07.30.html'
+    output_report_name = get_report_path(report_dir, last_log, logger)
+    assert str(output_report_name) == expected_report_name
