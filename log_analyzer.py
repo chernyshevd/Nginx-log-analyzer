4cadcd5a095ef2b7852ed140e030d244f190a676
chernyshevd <dmitrychernyshev@outlook.com>
2024-08-20 23:18:27 +0300

    fix

--- log_analyzer.py
+++ log_analyzer.py
@@ -18,14 +18,14 @@ from collections import defaultdict
 
 
 config = {
-    'TEMPLATE_PATH': './reports/report.html',  # Шаблон исходного отчета
-    'REPORT_SIZE': 100,  # Кол-во записей в итоговом отчете
-    'REPORT_DIR': './reports',  # Путь куда пишется отчет
-    'LOG_DIR': './log',  # Путь откуда читаются исходные логи
-    'ERRORS_LIMIT_PERC': 5,  # Допустимая ошибка парсинга в %
-    'SELF_LOG_PATH': './log/log_analyzer.log',  # Путь к собственныи логам программы
-    'DEBUG_MODE': True,
-    'TEST_SIZE': 1000
+    "TEMPLATE_PATH": "./reports/report.html",  # Шаблон исходного отчета
+    "REPORT_SIZE": 100,  # Кол-во записей в итоговом отчете
+    "REPORT_DIR": "./reports",  # Путь куда пишется отчет
+    "LOG_DIR": "./log",  # Путь откуда читаются исходные логи
+    "ERRORS_LIMIT_PERC": 5,  # Допустимая ошибка парсинга в %
+    "SELF_LOG_PATH": "./log/log_analyzer.log",  # Путь к собственныи логам программы
+    "DEBUG_MODE": True,
+    "TEST_SIZE": 1000,
 }
 
 
@@ -33,23 +33,37 @@ def parser_args() -> Dict[str, str]:
     """Парсер аргументов из командной строки"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', default='./config/config.json',
-                        help='Parameter for passing the config, default=./config/config.json')
+    parser.add_argument(
+        "--config",
+        default="./config/config.json",
+        help="Parameter for passing the config, default=./config/config.json",
+    )
     args = parser.parse_args()
 
     return args.config
 
 
-def get_result_config(default_config: Dict[str, str], config_path: str) -> Optional[Dict[str, str]]:
+def get_result_config(
+    default_config: Dict[str, str], config_path: str
+) -> Optional[Dict[str, str]]:
     """Получим результирующий конфиг"""
     try:
-        with open(config_path, 'r') as config_file:
+        with open(config_path, "r") as config_file:
             result_config = json.load(config_file)
-        return {**default_config, **result_config}  # Что бы не трогать дефолтный конфиг!
+        return {
+            **default_config,
+            **result_config,
+        }  # Что бы не трогать дефолтный конфиг!
     except ValueError:
         return None
 
-def logger_func(name: str, log_level: int = logging.DEBUG, stdout: bool = True, file: Optional[str] = None) -> logging.Logger:
+
+def logger_func(
+    name: str,
+    log_level: int = logging.DEBUG,
+    stdout: bool = True,
+    file: Optional[str] = None,
+) -> logging.Logger:
     """
     Создаем логгер
     """
@@ -62,7 +76,7 @@ def logger_func(name: str, log_level: int = logging.DEBUG, stdout: bool = True, 
         processors=[
             structlog.stdlib.add_log_level,
             structlog.processors.TimeStamper(fmt="iso"),
-            structlog.processors.JSONRenderer()
+            structlog.processors.JSONRenderer(),
         ],
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
@@ -80,8 +94,9 @@ def logger_func(name: str, log_level: int = logging.DEBUG, stdout: bool = True, 
     logging.getLogger().addHandler(stdout_handler)
     return logger
 
+
 def search_last_log(log_dir: str, logger: logging.Logger) -> Optional[Dict[str, Any]]:
-    '''
+    """
     Функция крайнего файла логгирования.
 
     Parameters
@@ -95,11 +110,11 @@ def search_last_log(log_dir: str, logger: logging.Logger) -> Optional[Dict[str, 
     -------
     result : Dict[str, str]
         Путь до файла лога
-    '''
+    """
 
-    logger.info(f'Search logs in directory {log_dir}')
+    logger.info(f"Search logs in directory {log_dir}")
 
-    regex = re.compile(r'nginx-access-ui.log-([\d]{8})(.*)')
+    regex = re.compile(r"nginx-access-ui.log-([\d]{8})(.*)")
 
     max_dt = datetime.datetime(1, 1, 1)
     max_path = None
@@ -108,8 +123,8 @@ def search_last_log(log_dir: str, logger: logging.Logger) -> Optional[Dict[str, 
     i = 0
     while i < len(file_lst):
         res = regex.search(file_lst[i])
-        if (res is not None) and (res.group(2) == '.gz' or res.group(2) == ''):
-            dt = datetime.datetime.strptime(res.group(1), '%Y%m%d')
+        if (res is not None) and (res.group(2) == ".gz" or res.group(2) == ""):
+            dt = datetime.datetime.strptime(res.group(1), "%Y%m%d")
             path = pathlib.Path(log_dir, file_lst[i])
             file = res.group(2)
             if dt > max_dt:
@@ -119,19 +134,19 @@ def search_last_log(log_dir: str, logger: logging.Logger) -> Optional[Dict[str, 
         i += 1
 
     if max_path is not None:
-        logger.info(f'The logs {max_path} was found')
-        return {'log_path': max_path, 'log_date': max_dt, 'log_file': max_file}
+        logger.info(f"The logs {max_path} was found")
+        return {"log_path": max_path, "log_date": max_dt, "log_file": max_file}
     else:
-        logger.info('The log file was not found!')
+        logger.info("The log file was not found!")
         return None
 
 
 def get_report_path(report_dir, last_log, logger):
-    '''
+    """
     Проверяем, существует ли отчет с таким именем в указанной dir.
     Если да, да парсинг выполнялся и прошел успешно возвращаем None, заканчиваем работу.
     Если нет, возвращаем path для записи отчета
-    '''
+    """
 
     report_name = f"report-{last_log['log_date'].strftime('%Y.%m.%d')}.html"
     report_path = pathlib.Path(report_dir, report_name)
@@ -144,29 +159,33 @@ def get_report_path(report_dir, last_log, logger):
 
 
 def parsing_line(line: str, logger: logging.Logger) -> Dict[str, Any]:
-    regex = re.compile(r'(?:GET|POST|HEAD|PUT|OPTIONS|DELETE).(.*).HTTP/.* (\d{1,6}[.]\d+)')
+    regex = re.compile(
+        r"(?:GET|POST|HEAD|PUT|OPTIONS|DELETE).(.*).HTTP/.* (\d{1,6}[.]\d+)"
+    )
     try:
         res_regex = regex.search(line)
     except UnicodeError:
-        logger.exception(f'Recording decoding error: {line}')
-        res = {'url': None, 'request_time': None, 'parsing_status': 'error'}
+        logger.exception(f"Recording decoding error: {line}")
+        res = {"url": None, "request_time": None, "parsing_status": "error"}
         return res
 
     if res_regex is None:
-        logger.debug(f'Record parsing error: {line}')
-        res = {'url': None, 'request_time': None, 'parsing_status': 'not_parsed'}
+        logger.debug(f"Record parsing error: {line}")
+        res = {"url": None, "request_time": None, "parsing_status": "not_parsed"}
         return res
 
     url = res_regex.group(1).strip()
     request_time = float(res_regex.group(2).strip())
-    res = {'url': url, 'request_time': request_time, 'parsing_status': 'success'}
+    res = {"url": url, "request_time": request_time, "parsing_status": "success"}
     return res
 
 
-def log_statistic_calc(latest_log: Dict, result_config: Dict, logger: logging.Logger) -> Optional[List[Dict]]:
+def log_statistic_calc(
+    latest_log: Dict, result_config: Dict, logger: logging.Logger
+) -> Optional[List[Dict]]:
 
-    log_path = latest_log['log_path']
-    log_file = latest_log['log_file']
+    log_path = latest_log["log_path"]
+    log_file = latest_log["log_file"]
 
     not_parsed_line = 0
     time_sum_all_req = 0
@@ -174,76 +193,85 @@ def log_statistic_calc(latest_log: Dict, result_config: Dict, logger: logging.Lo
     url_time_list = defaultdict(list)
 
     try:
-        opening_func = gzip.open if log_file == '.gz' else open
+        opening_func = gzip.open if log_file == ".gz" else open
     except OSError:
-        logger.error(f'The {log_path} file does not open')
+        logger.error(f"The {log_path} file does not open")
 
-    with opening_func(log_path, 'rb') as log_file:
+    with opening_func(log_path, "rb") as log_file:
         for line in log_file:
 
-            if result_config['DEBUG_MODE'] and logs_cnt == result_config['TEST_SIZE']:
+            if result_config["DEBUG_MODE"] and logs_cnt == result_config["TEST_SIZE"]:
                 break
             logs_cnt += 1
-            line = line.decode('utf-8')
+            line = line.decode("utf-8")
             parsed_line = parsing_line(line, logger)
-            if parsed_line['parsing_status'] == 'error':
-                logger.error('Error parsing logs')
+            if parsed_line["parsing_status"] == "error":
+                logger.error("Error parsing logs")
                 return None
-            elif parsed_line['parsing_status'] == 'not_parsed':
+            elif parsed_line["parsing_status"] == "not_parsed":
                 not_parsed_line += 1
                 continue
 
-            url = parsed_line['url']
-            request_time = parsed_line['request_time']
+            url = parsed_line["url"]
+            request_time = parsed_line["request_time"]
             time_sum_all_req += request_time
 
             url_time_list[url].append(request_time)
     url_stat = {}
 
-    logger.info(f'{logs_cnt} logs were found in the file {log_path}')
-    logger.info(f'{not_parsed_line} logs were not parsed')
+    logger.info(f"{logs_cnt} logs were found in the file {log_path}")
+    logger.info(f"{not_parsed_line} logs were not parsed")
 
     for url in url_time_list:
         url_stat_temp = {}
-        url_stat_temp['url'] = url
-        url_stat_temp['count'] = len(url_time_list[url])
-        url_stat_temp['count_perc'] = round(url_stat_temp['count'] / logs_cnt * 100, 3)
-        url_stat_temp['time_avg'] = round(statistics.mean(url_time_list[url]), 3)
-        url_stat_temp['time_max'] = round(max(url_time_list[url]), 3)
-        url_stat_temp['time_med'] = round(statistics.median(url_time_list[url]), 3)
-        url_stat_temp['time_sum'] = round(sum(url_time_list[url]), 3)
-        url_stat_temp['time_perc'] = round(url_stat_temp['time_sum'] / time_sum_all_req * 100, 3)
+        url_stat_temp["url"] = url
+        url_stat_temp["count"] = len(url_time_list[url])
+        url_stat_temp["count_perc"] = round(url_stat_temp["count"] / logs_cnt * 100, 3)
+        url_stat_temp["time_avg"] = round(statistics.mean(url_time_list[url]), 3)
+        url_stat_temp["time_max"] = round(max(url_time_list[url]), 3)
+        url_stat_temp["time_med"] = round(statistics.median(url_time_list[url]), 3)
+        url_stat_temp["time_sum"] = round(sum(url_time_list[url]), 3)
+        url_stat_temp["time_perc"] = round(
+            url_stat_temp["time_sum"] / time_sum_all_req * 100, 3
+        )
         url_stat[url] = url_stat_temp
 
     res_list = list(url_stat.values())
-    res_list = sorted(res_list, key=lambda x: x['time_sum'], reverse=True)
+    res_list = sorted(res_list, key=lambda x: x["time_sum"], reverse=True)
     return res_list
 
 
-def html_report_writer(result_config: str, report_path: str, logs_statistic: List[Dict], logger: logging.Logger) -> bool:
+def html_report_writer(
+    result_config: str,
+    report_path: str,
+    logs_statistic: List[Dict],
+    logger: logging.Logger,
+) -> bool:
     """
     Dump the statistics into a json string.
     Copy the report template to the dir specified in config with the desired name
     Rendering the report
     """
 
-    table_json = json.dumps(logs_statistic[:result_config['REPORT_SIZE']])
-    template_path = result_config['TEMPLATE_PATH']
+    table_json = json.dumps(logs_statistic[: result_config["REPORT_SIZE"]])
+    template_path = result_config["TEMPLATE_PATH"]
 
-    with open(report_path, 'w', encoding='utf-8') as report, open(template_path, 'r', encoding='utf-8') as tmpl:
+    with open(report_path, "w", encoding="utf-8") as report, open(
+        template_path, "r", encoding="utf-8"
+    ) as tmpl:
         tmpl_obj = string.Template(tmpl.read())
         try:
             render_html = tmpl_obj.safe_substitute(table_json=table_json)
         except Exception:
-            logger.error('The template could not be rendered!')
+            logger.error("The template could not be rendered!")
             return False
         report.write(render_html)
-        logger.info(f'The report {report_path} has been generated!')
+        logger.info(f"The report {report_path} has been generated!")
     return True
 
 
 def main():
-    '''
+    """
     1. Получаем результирующий config
     2. Создаем логера
     3. Проверяем параметры результирующего config
@@ -251,20 +279,19 @@ def main():
     5. Проверяем есть ли уже отчет в указанной папке, если находим конец
     6. Получаем статистику по логам
     7. Создаем отчет
-    '''
+    """
 
     config_path = parser_args()
-
 
     result_config = get_result_config(config, config_path)
 
     # log_path = result_config.get('SELF_LOG_PATH')
 
-    logger = logger_func(__name__, file=result_config['SELF_LOG_PATH'])
-    logger.info(f'Config path {config_path}')
+    logger = logger_func(__name__, file=result_config["SELF_LOG_PATH"])
+    logger.info(f"Config path {config_path}")
     logger.info(f'DEBUG_MODE {result_config["DEBUG_MODE"]}')
 
-    latest_log = search_last_log(result_config['LOG_DIR'], logger)
+    latest_log = search_last_log(result_config["LOG_DIR"], logger)
 
     if latest_log is not None:
 
@@ -276,13 +303,14 @@ def main():
                 logs_statistic = log_statistic_calc(latest_log, result_config, logger)
 
                 if logs_statistic is None:
-                    logger.debug('Statistics are empty!')
+                    logger.debug("Statistics are empty!")
                 else:
-                    html_report_writer(result_config, report_path, logs_statistic, logger)
+                    html_report_writer(
+                        result_config, report_path, logs_statistic, logger
+                    )
 
             except Exception:
-                logger.error('Emergency termination of the program!!!')
-
+                logger.error("Emergency termination of the program!!!")
 
 
 if __name__ == "__main__":
